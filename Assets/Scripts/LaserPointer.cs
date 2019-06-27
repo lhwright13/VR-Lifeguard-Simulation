38f9fa43e3d337f97af1c3d67ea594d2488f332f
Lucas Wright <lhwright13@gmail.com>
2019-06-26 23:31:04 -0500

    Laser Pointer and Player spawn

--- Assets/Scripts/LaserPointer.cs
+++ Assets/Scripts/LaserPointer.cs
@@ -6,13 +6,25 @@ public class LaserPointer : MonoBehaviour
 {
     [SerializeField]
     Material laserMaterial;
+
+    [SerializeField]
+    GameObject laserPointPrefab, indicatorPrefab;
+
+
+
+    private GameObject laserPoint;
+    private GameObject indicator;
+    private bool notFound = true;
+    
     // Start is called before the first frame update
     void Start()
     {
         LineRenderer lineRenderer = gameObject.AddComponent<LineRenderer>();
         lineRenderer.material = laserMaterial;
-        lineRenderer.widthMultiplier = 0.2f;
+        lineRenderer.widthMultiplier = 0.02f;
         lineRenderer.positionCount = 2;
+
+        laserPoint = Instantiate(laserPointPrefab);
     }
 
     // Update is called once per frame
@@ -25,23 +37,31 @@ public class LaserPointer : MonoBehaviour
 
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 100f))
         {
-            //call the laser to be made
             lineRenderer.SetPosition(0, transform.position);
             lineRenderer.SetPosition(1, hit.point);
 
+            //call the Laser point to be made
+            laserPoint.transform.position = hit.point;
+
             //if get the click key down 
-            if (hit.transform.gameObject.tag == "drowning")
+            if (hit.transform.gameObject.tag == "Drowning" && notFound)
             {
-                //stuff
+                indicator = Instantiate(indicatorPrefab);
+                indicator.transform.position = hit.transform.position + new Vector3(0,1,0);
+                //call coroutine in game manager after hit.
+                notFound = false;
             }
+            
 
         }
         else
         {
             lineRenderer.SetPosition(0, transform.position);
             lineRenderer.SetPosition(1, transform.position + transform.forward*100f);
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
-            Debug.Log("Did not Hit");
+            lineRenderer.material = laserMaterial;
+            lineRenderer.widthMultiplier = 0.02f;
+            lineRenderer.positionCount = 2;
+            laserPoint.transform.position = transform.position + transform.forward * 100f;
         }
     }
 }
