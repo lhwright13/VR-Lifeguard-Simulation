cddac1d194d69336e3cb02f1cdc73510ae6eeec6
Lucas Wright <lhwright13@gmail.com>
2019-06-27 09:34:26 -0500

    the spawner now has no overlaps

--- Assets/Scripts/PeopleManager.cs
+++ Assets/Scripts/PeopleManager.cs
@@ -30,8 +30,17 @@ public class PeopleManager : MonoBehaviour
             int personChoice = Random.Range(0, peoplePrefabs.Length - 1);
             Vector3 spawnPos = new Vector3(Random.Range(xRange.x, xRange.y), 0, Random.Range(yRange.x, yRange.y));
             Vector3 rot = new Vector3(0, Random.Range(0, 359),0);
+            float sphereRadius = 1.2f;
 
-            Instantiate(peoplePrefabs[personChoice], spawnPos, Quaternion.Euler(rot));
+            if (Physics.CheckSphere(spawnPos, sphereRadius))
+            {
+                print("not here");
+                i--;
+            }
+            else
+            {
+                Instantiate(peoplePrefabs[personChoice], spawnPos, Quaternion.Euler(rot));
+            }
         }
     }
 }
