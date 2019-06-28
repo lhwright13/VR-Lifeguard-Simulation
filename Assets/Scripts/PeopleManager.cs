8bd3be91a9b3f408cd97faf5b4a2531fcebdded8
Lucas Wright <lhwright13@gmail.com>
2019-06-28 10:16:34 -0500

    put things in beach scene

--- Assets/Scripts/PeopleManager.cs
+++ Assets/Scripts/PeopleManager.cs
@@ -28,18 +28,17 @@ public class PeopleManager : MonoBehaviour
         for(int i = 0; i < number; i++)
         {
             int personChoice = Random.Range(0, peoplePrefabs.Length - 1);
-            Vector3 spawnPos = new Vector3(Random.Range(xRange.x, xRange.y), 0, Random.Range(yRange.x, yRange.y));
+            Vector3 spawnPos = new Vector3(Random.Range(xRange.x, xRange.y), 1, Random.Range(yRange.x, yRange.y));
             Vector3 rot = new Vector3(0, Random.Range(0, 359),0);
             float sphereRadius = 1.2f;
 
             if (Physics.CheckSphere(spawnPos, sphereRadius))
             {
-                print("not here");
                 i--;
             }
             else
             {
-                Instantiate(peoplePrefabs[personChoice], spawnPos, Quaternion.Euler(rot));
+                Instantiate(peoplePrefabs[personChoice], spawnPos + new Vector3 (0,-0.4f,0), Quaternion.Euler(rot));
             }
         }
     }
