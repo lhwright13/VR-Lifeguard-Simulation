62da26d7d29ae289318a7fbd4c8a52eb2ddb2336
Lucas Wright <lhwright13@gmail.com>
2019-07-06 12:29:33 -0500

    The smoke Toggles with keypress "S"

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,8 @@ public class GameManager : MonoBehaviour
     private GameObject[] people;
     public PlayerControler landNav;
     public InstructionDisp display;
+    private GameObject[] smokes;
+    bool smokeOn = false;
 
     void Start()
     {
@@ -15,12 +17,34 @@ public class GameManager : MonoBehaviour
 
         //start coroutine for drowning player between 2 and 10 seconds
         Invoke("Drown", Random.Range(2f, 10f));
+        //turn the smoke off by default:
+        smokes = GameObject.FindGameObjectsWithTag("Smoke");
+        foreach(GameObject smoke in smokes)
+        {
+            smoke.SetActive(false);
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.S) && !smokeOn)
+        {
+            foreach (GameObject smoke in smokes)
+            {
+                smoke.SetActive(true);
+                smokeOn = true;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.A) && smokeOn)
+        {
+            foreach (GameObject smoke in smokes)
+            {
+                smoke.SetActive(false);
+                smokeOn = false;
+            }
+        }
     }
     private void Drown()
     {
